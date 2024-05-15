01946f3f309e28ae9f61706e41848a7a01058af7
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-05-15 12:00:13 -0500

    Made some of the UI and got most of the stlying done.

---
+++ src/components/Weather.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { MainContainer } from "./styles.module";
+import { IoSearchOutline } from "react-icons/io5";
+import { WiHumidity } from "react-icons/wi";
+import { WiStrongWind } from "react-icons/wi";
+
+const Weather = () => {
+  return (
+    <MainContainer>
+      <div className="container">
+        <div className="searchArea">
+          <input type="text" placeholder="Please enter a city" />
+
+          <div className="searchCircle">
+            <IoSearchOutline className="searchIcon" />
+          </div>
+        </div>
+
+        <div className="weatherContainer">
+          <h1>City</h1>
+          <span>idk</span>
+          <div className="icon">icon</div>
+          <h1>95F</h1>
+          <h2>Cloudy</h2>
+        </div>
+        <div className="infoArea">
+          <div className="humidity">
+            <WiHumidity className="Icon" />
+            <div className="Info">
+              <h1>60%</h1>
+              <p>Humidity</p>
+            </div>
+          </div>
+
+          <div className="wind">
+            <WiStrongWind className="Icon" />
+            <div className="Info">
+              <h1>2.35km/hr</h1>
+              <p>wind speed</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </MainContainer>
+  );
+};
+
+export default Weather;
